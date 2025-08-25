aec79b7f68608e3157a73d620dbb4717ab6cd66e
Tobias Fischer <tobias.fischer@gbv.de>
2025-08-25 14:17:02 +0000

    update timerange check for updater

--- src/updater/GettyUpdater.js
+++ src/updater/GettyUpdater.js
@@ -12,7 +12,7 @@ let info = {}
 let access_token = '';
 
 if (process.argv.length >= 3) {
-    info = JSON.parse(process.argv[2])
+  info = JSON.parse(process.argv[2])
 }
 
 function hasChanges(objectOne, objectTwo) {
@@ -28,25 +28,37 @@ function hasChanges(objectOne, objectTwo) {
 }
 
 function getConfigFromAPI() {
-        return new Promise((resolve, reject) => {
-                var url = 'http://fylr.localhost:8081/api/v1/config?access_token=' + access_token
-                fetch(url, {
-                                headers: {
-                                        'Accept': 'application/json'
-                                },
-                        })
-                        .then(response => {
-                                if (response.ok) {
-                                        resolve(response.json());
-                                } else {
-                                        console.error("Getty-Updater: Fehler bei der Anfrage an /config ");
-                                }
-                        })
-                        .catch(error => {
-                                console.error(error);
-                                console.error("Getty-Updater: Fehler bei der Anfrage an /config");
-                        });
-        });
+  return new Promise((resolve, reject) => {
+    var url = 'http://fylr.localhost:8081/api/v1/config?access_token=' + access_token
+    fetch(url, {
+      headers: {
+        'Accept': 'application/json'
+      },
+    })
+      .then(response => {
+        if (response.ok) {
+          resolve(response.json());
+        } else {
+          console.error("Getty-Updater: Fehler bei der Anfrage an /config ");
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        console.error("Getty-Updater: Fehler bei der Anfrage an /config");
+      });
+  });
+}
+
+function isInTimeRange(currentHour, fromHour, toHour) {
+  if (fromHour === toHour) {
+    return true;
+  }
+
+  if (fromHour < toHour) { // same day
+    return currentHour >= fromHour && currentHour < toHour;
+  } else { // through the night
+    return currentHour >= fromHour || currentHour < toHour;
+  }
 }
 
 main = (payload) => {
@@ -79,14 +91,14 @@ main = (payload) => {
         let dataRequestUrl = 'https://jsontojsonp.gbv.de/?url=' + encodeURIComponent(uri) + '.json'
         let dataRequest = fetch(dataRequestUrl);
         requests.push({
-            url: dataRequestUrl,
-            uri: uri,
-            request: dataRequest
+          url: dataRequestUrl,
+          uri: uri,
+          request: dataRequest
         });
         requestUrls.push(dataRequest);
       });
 
-      Promise.all(requestUrls).then(function(responses) {
+      Promise.all(requestUrls).then(function (responses) {
         let results = [];
         // Get a JSON object from each of the responses
         responses.forEach((response, index) => {
@@ -106,7 +118,7 @@ main = (payload) => {
           results.push(result);
         });
         return Promise.all(results.map(result => result.data));
-      }).then(function(data) {
+      }).then(function (data) {
         let results = [];
         data.forEach((data, index) => {
           let url = requests[index].url;
@@ -143,7 +155,7 @@ main = (payload) => {
             if (resultJSON) {
               // get desired language for preflabel. This is frontendlanguage from original data...
               let desiredLanguage = 'de';
-              if(originalCdata?.frontendLanguage?.length == 2) {
+              if (originalCdata?.frontendLanguage?.length == 2) {
                 desiredLanguage = originalCdata.frontendLanguage;
               }
               // save conceptName
@@ -152,7 +164,7 @@ main = (payload) => {
               newCdata.conceptURI = resultJSON.id;
               // save conceptGeoJSON
               let conceptGeoJSON = GettyUtil.getGeoJSONFromGettyJSON(resultJSON);
-              if(conceptGeoJSON)
+              if (conceptGeoJSON)
                 newCdata.conceptGeoJSON = conceptGeoJSON;
               // save conceptSource
               var conceptSource = (resultJSON.id.match(/http:\/\/vocab\.getty\.edu\/([^\/]+)/) || [])[1] || null;
@@ -165,7 +177,7 @@ main = (payload) => {
               newCdata.frontendLanguage = desiredLanguage;
               if (hasChanges(payload.objects[index].data, newCdata)) {
                 payload.objects[index].data = newCdata;
-              } else {}
+              } else { }
             }
           } else {
             console.error('No matching record found');
@@ -228,7 +240,7 @@ outputErr = (err2) => {
       const now = new Date();
       const hour = now.getHours();
       // check if hours do not match
-      if (hour < getty_config.from_time && hour >= getty_config.to_time) {
+      if (!isInTimeRange(hour, getty_config.from_time, getty_config.to_time)) {
         // exit if hours do not match
         outputData({
           "state": {
@@ -239,74 +251,74 @@ outputErr = (err2) => {
       }
     }
   }
-  
+
   access_token = info && info.plugin_user_access_token;
 
-  if(access_token) {
+  if (access_token) {
+
+    ////////////////////////////////////////////////////////////////////////////
+    // get config and read the languages
+    ////////////////////////////////////////////////////////////////////////////
+
+    getConfigFromAPI().then(config => {
+      databaseLanguages = config.system.config.languages.database;
+      databaseLanguages = databaseLanguages.map((value, key, array) => {
+        return value.value;
+      });
+
+      frontendLanguages = config.system.config.languages.frontend;
+
 
       ////////////////////////////////////////////////////////////////////////////
-      // get config and read the languages
+      // availabilityCheck for k10plus-api
       ////////////////////////////////////////////////////////////////////////////
 
-      getConfigFromAPI().then(config => {
-          databaseLanguages = config.system.config.languages.database;
-          databaseLanguages = databaseLanguages.map((value, key, array) => {
-            return value.value;
-          });
+      encodedGettyURL = encodeURIComponent('https://vocab.getty.edu/tgn/7000084.json')
+      testURL = 'https://jsontojsonp.gbv.de/?url=' + encodedGettyURL
 
-          frontendLanguages = config.system.config.languages.frontend;
-
-
-          ////////////////////////////////////////////////////////////////////////////
-          // availabilityCheck for k10plus-api
-          ////////////////////////////////////////////////////////////////////////////
-
-          encodedGettyURL = encodeURIComponent('https://vocab.getty.edu/tgn/7000084.json')
-          testURL = 'https://jsontojsonp.gbv.de/?url=' + encodedGettyURL
-
-          https.get(testURL, res => {
-            let testData = [];
-            res.on('data', chunk => {
-              testData.push(chunk);
-            });
-            res.on('end', () => {
-              testData = Buffer.concat(testData).toString();
-              const testJSON = JSON.parse(testData);
-              if (testJSON && testData.includes('"Deutschland"')) {
-                ////////////////////////////////////////////////////////////////////////////
-                // test successfull --> continue with custom-data-type-update
-                ////////////////////////////////////////////////////////////////////////////
-                process.stdin.on('readable', () => {
-                  let chunk;
-                  while ((chunk = process.stdin.read()) !== null) {
-                    data = data + chunk
-                  }
-                });
-                process.stdin.on('end', () => {
-                  ///////////////////////////////////////
-                  // continue with update-routine
-                  ///////////////////////////////////////
-                  try {
-                    let payload = JSON.parse(data)
-                    main(payload)
-                  } catch (error) {
-                    console.error("caught error", error)
-                    outputErr(error)
-                  }
-                });
-              } else {
-                console.error('Error while interpreting data from Getty-API.');
+      https.get(testURL, res => {
+        let testData = [];
+        res.on('data', chunk => {
+          testData.push(chunk);
+        });
+        res.on('end', () => {
+          testData = Buffer.concat(testData).toString();
+          const testJSON = JSON.parse(testData);
+          if (testJSON && testData.includes('"Deutschland"')) {
+            ////////////////////////////////////////////////////////////////////////////
+            // test successfull --> continue with custom-data-type-update
+            ////////////////////////////////////////////////////////////////////////////
+            process.stdin.on('readable', () => {
+              let chunk;
+              while ((chunk = process.stdin.read()) !== null) {
+                data = data + chunk
               }
             });
-          }).on('error', err => {
-            console.error('Error while receiving data from Getty-API: ', err.message);
-          });
-      }).catch(error => {
-        console.error('Es gab einen Fehler beim Laden der Konfiguration:', error);
+            process.stdin.on('end', () => {
+              ///////////////////////////////////////
+              // continue with update-routine
+              ///////////////////////////////////////
+              try {
+                let payload = JSON.parse(data)
+                main(payload)
+              } catch (error) {
+                console.error("caught error", error)
+                outputErr(error)
+              }
+            });
+          } else {
+            console.error('Error while interpreting data from Getty-API.');
+          }
+        });
+      }).on('error', err => {
+        console.error('Error while receiving data from Getty-API: ', err.message);
       });
-    }
-    else {
-        console.error("kein Accesstoken gefunden");
-    }
+    }).catch(error => {
+      console.error('Es gab einen Fehler beim Laden der Konfiguration:', error);
+    });
+  }
+  else {
+    console.error("kein Accesstoken gefunden");
+  }
 
 })();
